2c3642fa01e7b6f655c865aca539198b81b2b53a
grog8355 <gevorg.gevorgyan.be@gmail.com>
2024-11-17 03:10:46 +0100

    Text sizing fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,7 +44,7 @@
 
 <main class="bg-gradient-to-b from-white from-30% via-blue-300 to-blue-500 w-screen h-screen font-Inter lg:mt-10">
     <section>
-        <article class="flex flex-col justify-center m-7 text-4xl sm:text-6xl lg:8xl font-semibold text-center gap-10">
+        <article class="flex flex-col justify-center m-7 text-3xl sm:text-6xl lg:8xl font-semibold text-center gap-10">
             <h1 class="drop-shadow">Free and reliable P2P <span class="text-blue-500">Bittorrent</span> client</h1>
 
             <div class="flex flex-col items-center w-[100%]">
@@ -54,7 +54,7 @@
                             <a href="{image}" target="_blank"><img class="max-h-[35rem] lg:max-w-[56rem]" src="{image}" alt="{alt}"></a>
                         {/if}
                     {/each}
-                    <div class="flex justify-between w-[100%]">
+                    <div class="flex justify-between w-[100%] gap-1">
                         <button class="flex justify-center rounded bg-white shadow-md w-36 sm:w-56 hover:shadow-med active:bg-opacity-55" onclick={() => changeImg(1)}><img class="max-w-10 sm:max-w-16" src="{lArrow}" alt="left arrow"></button>
                         <button class="flex justify-center rounded bg-white shadow-md w-36 sm:w-56 hover:shadow-med active:bg-opacity-55" onclick={() => changeImg(2)}><img class="max-w-10 sm:max-w-16" src="{rArrow}" alt="right arrow"></button>               
                     </div>
